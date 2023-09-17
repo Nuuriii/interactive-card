02d08f607d6a778233fea546a3717f5f4159e667
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-09-17 17:15:22 +0700

    feat: use Space Grotesk font to project

--- src/components/Wrapper/wrapper.style.ts
+++ src/components/Wrapper/wrapper.style.ts
@@ -1,10 +1,17 @@
 import styled from 'styled-components';
+import SpaceGrotesk from '../../assets/fonts/SpaceGrotesk-VariableFont_wght.ttf';
 
 export const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   height: 100vh;
+  font-family: SpaceGrotesk, serif;
+
+  @font-face {
+    font-family: SpaceGrotesk;
+    src: url(${SpaceGrotesk});
+  }
 `;
 
 export const Wrapper = styled.div`
